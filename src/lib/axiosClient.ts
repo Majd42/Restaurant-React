6db978c047542f4cfd972e1b559ac6c990f0600e
Majd Shaheen <majd4.sh@gmail.com>
2025-02-24 11:09:49 +0400

    fix loading issue

--- src/lib/axiosClient.ts
+++ src/lib/axiosClient.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_BASE_API_URL,
+  // baseURL: import.meta.env.VITE_BASE_API_URL,
+  baseURL: "https://stg.tdh.start-tech.ae/api",
   timeout: 30000,
 });
 
